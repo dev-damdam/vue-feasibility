ab68149a7c715e7eb2a3c19b9df5feef909510ab
dev-damdam <dev.sodam@gmail.com>
2022-09-16 01:12:15 +0900

    add markdown project

---
+++ vue-markdown/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <markdown-sample />
+  </div>
+</template>
+
+<script>
+import MarkdownSample from "./components/markdown-sample.vue";
+
+export default {
+  name: "App",
+  components: {
+    MarkdownSample,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-markdown/src/components/markdown-sample.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="markdown-sample-wrapper">
+    <textarea v-model="markdown" />
+    <span>마크다운 결과</span>
+    <div v-html="markdownRes" />
+  </div>
+</template>
+
+<script>
+import { marked } from "marked";
+export default {
+  name: "markdown-sample",
+  data() {
+    return {
+      markdown: "",
+    };
+  },
+  computed: {
+    markdownRes: {
+      set(value) {
+        this.markdown = value;
+      },
+      get() {
+        return marked(this.markdown, {
+          sanitize: true,
+        });
+      },
+    },
+  },
+  mounted() {},
+};
+</script>
+<style scoped lang="scss">
+.markdown-sample-wrapper {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  gap: 10px;
+}
+</style>
